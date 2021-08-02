95ec33b01e47aa9556bbb04166d0c277c6705181
Evert Timberg <evert.timberg@gmail.com>
2021-08-02 19:16:28 -0400

    [chore]: Add tasks for starting and stopping local DB

--- tasks/__init__.py
+++ tasks/__init__.py
@@ -2,6 +2,7 @@ from invoke import Collection
 
 from . import (
     build,
+    database,
     lint,
     test,
 )
@@ -11,6 +12,8 @@ ns = Collection()
 ns.add_task(build.dist, name="build")
 ns.add_task(build.requirements)
 ns.add_task(build.release)
+ns.add_task(database.local_db)
+ns.add_task(database.local_db_stop)
 ns.add_task(test.test)
 # Module collection tasks
 ns.add_collection(lint)
