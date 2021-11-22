4576a31fe69f3a9f27ee66c19a1fd26b531bdd3c
Evert Timberg <evert.timberg@tophatmonocle.com>
2021-11-22 09:28:32 -0500

    WIP: Add broken test for array contains

--- tests/fields/test_array.py
+++ tests/fields/test_array.py
@@ -3,6 +3,7 @@ from typing import Optional
 import ormar
 import pytest
 from sqlalchemy import String
+from sqlalchemy.dialects import postgresql
 
 from ormar_postgres_extensions.fields import Array
 from tests.database import (
@@ -46,3 +47,17 @@ async def test_create_model_with_nullable_array(db):
 
     found = await NullableArrayTestModel.objects.get()
     assert found.data is None
+
+
+@pytest.mark.asyncio
+async def test_filter_array_column(db):
+    await ArrayTestModel(data=["a", "b"]).save()
+    await ArrayTestModel(data=["c"]).save()
+
+    print(ArrayTestModel.data)
+
+    found = await ArrayTestModel.objects.filter(
+        (ArrayTestModel.data.contains(["c"]))
+    ).get()
+
+    assert len(found) == 1
